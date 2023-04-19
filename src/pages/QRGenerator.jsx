660996b688e2abbf996de3d805ce087e4c82cc4d
Jhonas Palad <jhonasemmanuel@gmail.com>
2023-04-19 15:07:36 +0800

    new commit

--- src/pages/QRGenerator.jsx
+++ src/pages/QRGenerator.jsx
@@ -4,40 +4,43 @@ import { v4 as uuidv4 } from 'uuid';
 
 import AddRemoveInput from '../components/AddRemoveInput';
 // import QRCode from '../components/QRCode';
-import {QRCode} from 'react-qrcode-logo';
+import QRCodeLogo from '../components/QRCodeLogo';
 import Container from '../components/Container';
 import ButtonSelect from '../components/ButtonSelect';
 import FormGroup from '../components/FormGroup';
 import InputRemoveSelect from '../components/InputRemoveSelect';
 import AddressAddRemoveInput from '../components/AddressAddRemoveInput';
 import ADRInputRemoveSelect from '../components/ADRInputRemoveSelect';
+import { QRCode } from 'react-qrcode-logo';
 
 const initState = {
     inputs: {
-        addresses: [
-            {
-                id: uuidv4(),
-                street: '',
-                city: '',
-                state: '',
-                zip: '4232',
-                country: 'Philippines',
-                type: '',
-                pref: 1
-            }
-        ],
-        telephones: [
-            {id: 'work', value: '', type: 'work', _default: true},
-        ],
-        emails: [],
-        urls: [],
         fname: '',
-        mname: '',
         lname: '',
-        org: '',
-        title: '',
-        role: '',
-        
+        cellphone: '',
+        email: '',
+        company: '',
+        designation: '',
+        websites: [],
+        landlines: [],
+        // addresses: [
+        //     {
+        //         id: uuidv4(),
+        //         street: '',
+        //         city: '',
+        //         state: '',
+        //         zip: '4232',
+        //         country: 'Philippines',
+        //         type: '',
+        //         pref: 1
+        //     }
+        // ],
+        // telephones: [
+        //     {id: uuidv4(), value: '', type: 'work', _default: true},
+        // ],
+        // org: '',
+        // title: '',
+        // role: '',
     },
     toConvert: '',
     fileType: 'png',
@@ -49,83 +52,27 @@ const reducer = (state, action) => {
     const {type, payload} = action;
     const {inputs} = state;
     switch(type) {
-        case 'REMOVE_ADDRESS':
-            //Payload = id
-            const removedAddresses = inputs.addresses.filter((input)=> input.id !== payload.id)
-            return {...state,inputs: {...inputs,addresses: removedAddresses}}
-        case 'ADD_ADDRESS':
-            //Payload = {id, type, value}
-            const newAddress = {id:payload.id, type: '', street: '', city: '', zip: '4232', state: '', country: 'Philippines'}
-            return {...state, inputs: {...inputs,addresses: [...inputs.addresses, newAddress]}}
-        case 'UPDATE_ADDRESS_INPUT':
-            //Payload = {id,name, value}
-            const inputAddresses = [...inputs.addresses];
-            const addressInput = inputAddresses.find((input)=> input.id === payload.id);
-            addressInput[payload.name] = payload.value;
-            return {...state, inputs: {...inputs,addresses: inputAddresses}};
-        case 'UPDATE_ADDRESS_TYPE':
-            const typeAddresses = [...inputs.addresses];
-            const addressType = typeAddresses.find((input)=> input.id === payload.id);
-            addressType.type = payload.value;
-            return {...state, inputs: {...inputs, addresses: typeAddresses}};
-        
-        case 'REMOVE_TELEPHONE':
-            //Payload = id
-            const removedTelephones = inputs.telephones.filter((input)=> input.id !== payload.id)
-            return {...state,inputs: {...inputs,telephones: removedTelephones}}
-        case 'ADD_TELEPHONE':
-            //Payload = {id, type, value}
-            return {...state,inputs: { ...inputs,telephones: [...inputs.telephones, payload]}}
-        case 'UPDATE_TELEPHONE_INPUT':
-            //Payload = {id, value}
-            const inputTelephones = [...inputs.telephones];
-            const telephoneInput = inputTelephones.find((input)=> input.id === payload.id);
-            telephoneInput.value = payload.value;
-            return {...state, inputs: {...inputs,telephones: inputTelephones}};
-        case 'UPDATE_TELEPHONE_TYPE':
-            const typeTelephones = [...inputs.telephones];
-            const telephoneType = typeTelephones.find((input)=> input.id === payload.id);
-            telephoneType.type = payload.value;
-            return {...state, inputs: {...inputs,telephones: typeTelephones}};
-        
+        case 'ADD_WEBSITES':
+            const newWebsites = [...inputs.websites, {id: uuidv4(), value: ''}]
+            return {...state, inputs: {...inputs, websites: newWebsites}}
+        case 'ADD_LANDLINES' : 
+            const newLandlines = [...inputs.landlines, { id: uuidv4(), value: ''}]
+            return {...state, inputs: {...inputs, landlines: newLandlines}}
+        case 'UPDATE_WEBSITES_INPUT':
+            const inputWebsites = [...inputs.websites];
+            const websiteInput = inputWebsites.find((input) => input.id === payload.id);
+            websiteInput.value = payload.value;
+            return {...state, inputs: {...inputs, websites: inputWebsites}} 
+        case 'UPDATE_LANDLINES_INPUT':
+            const inputLandlines = [...inputs.landlines];
+            const landlineInput = inputLandlines.find((input) => input.id === payload.id);
+            landlineInput.value = payload.value;
+            return {...state, inputs: {...inputs, landlines: inputLandlines}} 
         case 'REMOVE_EMAIL':
             //Payload = id
             const removedEmails = inputs.emails.filter((input)=> input.id !== payload.id)
             return {...state,inputs: {...inputs,emails: removedEmails}}
-        case 'ADD_EMAIL':
-            //Payload = {id, type, value}
-            return {...state, inputs: {...inputs, emails: [...inputs.emails, payload]}}
-        case 'UPDATE_EMAIL_INPUT':
-            //Payload = {id, value}
-            const inputEmails = [...inputs.emails];
-            const emailInput = inputEmails.find((input)=> input.id === payload.id);
-            emailInput.value = payload.value;
-            return {...state, inputs: {...inputs,emails: inputEmails}};
-        case 'UPDATE_EMAIL_TYPE':
-            const typeEmails = [...inputs.emails];
-            const emailType = typeEmails.find((input)=> input.id === payload.id);
-            emailType.type = payload.value;
-            return {...state, inputs: {...inputs,emails: typeEmails}};
-        
-        case 'REMOVE_URL':
-            //Payload = id
-            const removedUrls = inputs.urls.filter((input)=> input.id !== payload.id)
-            return {...state, inputs: {...inputs,urls: removedUrls}}
-        case 'ADD_URL':
-            //Payload = {id, type, value}
-            return {...state, inputs: {...inputs, urls: [...inputs.urls, payload]}}
-        case 'UPDATE_URL_INPUT':
-            //Payload = {id, value}
-            const inputUrls = [...inputs.urls];
-            const urlInput = inputUrls.find((input)=> input.id === payload.id);
-            urlInput.value = payload.value;
-            return {...state, inputs: {...inputs,urls: inputUrls}};
-        case 'UPDATE_URL_TYPE':
-            const typeUrls = [...inputs.urls];
-            const urlType = typeUrls.find((input)=> input.id === payload.id);
-            urlType.type = payload.value;
-            return {...state, inputs: {...inputs, urls: typeUrls}};
-        
+
         case 'INPUT_ONCHANGE':
             const inputUpdatedState = {...inputs};
             inputUpdatedState[payload.id] = payload.value;
@@ -136,16 +83,24 @@ const reducer = (state, action) => {
                 fileType: action.value
             }
         case 'GENERATEQR':
-            const {lname, fname, telephones, addresses, urls, emails} = state.inputs;
-            const FN = makeFormat('FN', {fname, mname: 'Olfato', lname});
+            const {fname, lname, cellphone, email ,company, designation, websites, landlines} = state.inputs;
+            
+            const FN = makeFormat('FN', {fname, lname});
             const N = makeFormat('N', {lname, fname});
-            const formattedTelNums = telephones.length ? makeFormat('TEL', telephones) + '\r\n' : '';
-            const formattedAddresses = addresses.length ? makeFormat('ADR', addresses) + '\r\n' : '';
-            const formattedUrls = urls.length ? makeFormat('URL', urls)+ '\r\n' : '';
-            const formattedEmails = emails.length ? makeFormat('EMAIL', emails) + '\r\n' : '';
-            const vCardFormat = `BEGIN:VCARD\r\nVERSION:3.0\r\n${FN}${N}${formattedTelNums}${formattedEmails}${formattedAddresses}${formattedUrls}END:VCARD`;
-        
+            const ORG = makeFormat('ORG', company);
+            const EMAIL = makeFormat('EMAIL', email);
+            const TEL = makeFormat('TEL', cellphone);
+            const TITLE = makeFormat('TITLE', designation);
+            const X_SOCIAL_PROFILES = websites.length ? makeFormat('X-SOCIAL-PROFILES', websites) + '\r\n' : '';
+            // const TITLE = makeFormat('TITLE', title);
+            // const ROLE = makeFormat('ROLE', role);
+            // const formattedTelNums = telephones.length ? makeFormat('TEL', telephones) + '\r\n' : '';
+            // const formattedAddresses = addresses.length ? makeFormat('ADR', addresses) + '\r\n' : '';
+            // const formattedUrls = urls.length ? makeFormat('URL', websites)+ '\r\n' : '';
+            // const formattedEmails = emails.length ? makeFormat('EMAIL', emails) + '\r\n' : '';
+            const vCardFormat = `BEGIN:VCARD\r\nVERSION:3.0\r\n${FN}${N}${ORG}${TITLE}${TEL}${EMAIL}${X_SOCIAL_PROFILES}END:VCARD`;
             console.log(vCardFormat);
+            
             return {
                 ...state,
                 showDownload:true,
@@ -178,33 +133,23 @@ function QRGenerator () {
     const handleAddInput = useCallback((actionType) => {
         dispatch({type: actionType, payload: {id: uuidv4(), type:'', value:''}})
     }, []);
-    const handleRemoveInput = useCallback((id, actionType) => {
-        dispatch({type: actionType, payload:{id}})
-    }, []);
+    // const handleRemoveInput = useCallback((id, actionType) => {
+    //     dispatch({type: actionType, payload:{id}})
+    // }, []);
     console.log(state);
     return (
         <Container style={{flexGrow:1}} className="body">
                 <h1 style={{fontSize:27, fontWeight:'bold',textAlign: 'center'}}>
-                    vCard QR CODE <br/>
+                    VCard<br/>
                     GENERATOR
                 </h1>
                 <div className='container'>
-                <div style={{gap:'1em'}} className='flex flex-row'>
-                    
                     <FormGroup
                         id="fname"
                         label="First Name"
                         name="fname"
                         value={state.inputs.fname}
                         onChange={(e)=>handleChangeInput(e,'INPUT_ONCHANGE')}
-
-                    />
-                    <FormGroup
-                        id="mname"
-                        label="Middle Name"
-                        name="mname"
-                        value={state.inputs.mname}
-                        onChange={(e)=>handleChangeInput(e, 'INPUT_ONCHANGE')}
                     />
                     <FormGroup
                         id="lname"
@@ -213,95 +158,51 @@ function QRGenerator () {
                         value={state.inputs.lname}
                         onChange={(e)=>handleChangeInput(e,'INPUT_ONCHANGE')}
                     />
-
-                </div>
-                <div style={{gap:'1em'}} className='flex flex-row'>
+                    
                     <FormGroup
-                        id="org"
-                        label="Organization"
-                        name="org"
-                        value={state.inputs.org}
-                        onChange={(e)=>handleChangeInput(e,'INPUT_ONCHANGE')}
-                    />
-                    <FormGroup
-                        id="title"
-                        label="Title"
-                        name="title"
+                        id="cellphone"
+                        label="Cellphone Number"
+                        name="cellphone"
                         value={state.inputs.title}
                         onChange={(e)=>handleChangeInput(e,'INPUT_ONCHANGE')}
                     />
                     <FormGroup
-                        id="role"
-                        label="Role"
-                        name="role"
-                        value={state.inputs.role}
-                        onChange={(e)=>handleChangeInput(e,'INPUT_ONCHANGE')}
+                        id="email"
+                        label="E-mail address"
+                        value={state.inputs.email}
+                        onChange={(e)=>handleChangeInput(e, 'INPUT_ONCHANGE')}
                     />
-                </div>
-                <div>
-                    <h3>
-                        Telephone
-                    </h3>
-                    <InputRemoveSelect
-                        type="work"
-                        id="work"
-                        value={state.inputs.telephones[0].value}
-                        onChangeType={(e)=>handleChangeInput(e, 'UPDATE_TELEPHONE_TYPE')}
-                        onChange={(e)=>handleChangeInput(e, 'UPDATE_TELEPHONE_INPUT')}
+                    <FormGroup
+                        id="company"
+                        label="Company"
+                        value={state.inputs.company}
+                        onChange={(e)=>handleChangeInput(e, 'INPUT_ONCHANGE')}
                     />
-                    <AddRemoveInput
-                        labels={["mobile","phone","fax"]}
-                        container={state.inputs.telephones}
-                        onChangeType={(e)=>handleChangeInput(e, 'UPDATE_TELEPHONE_TYPE')}
-                        onChange={(e)=>handleChangeInput(e, 'UPDATE_TELEPHONE_INPUT')}
-                        addInput={()=>handleAddInput('ADD_TELEPHONE')} 
-                        removeInput={(id)=>handleRemoveInput(id, 'REMOVE_TELEPHONE')}
+                    <FormGroup
+                        id="designation"
+                        label="Designation"
+                        value={state.inputs.designation}
+                        onChange={(e)=>handleChangeInput(e, 'INPUT_ONCHANGE')}
                     />
-                </div>
                 <AddRemoveInput
-                    title="Email" 
-                    container={state.inputs.emails}
-                    onChangeType={(e)=>handleChangeInput(e, 'UPDATE_EMAIL_TYPE')}
-                    onChange={(e)=>handleChangeInput(e, 'UPDATE_EMAIL_INPUT')}
-                    addInput={()=>handleAddInput('ADD_EMAIL')} 
-                    removeInput={(id)=>handleRemoveInput(id, 'REMOVE_EMAIL')}
+                    title="Landline Number" 
+                    container={state.inputs.landlines}
+                    limit={1}
+                    // onChangeType={(e)=>handleChangeInput(e, 'UPDATE_EMAIL_TYPE')}
+                    onChange={(e)=>handleChangeInput(e, 'UPDATE_LANDLINES_INPUT')}
+                    addInput={()=>handleAddInput('ADD_LANDLINES')} 
+                    // removeInput={(id)=>handleRemoveInput(id, 'REMOVE_EMAIL')}
                 />
                 <AddRemoveInput
-                    title="URLS" 
-                    container={state.inputs.urls}
-                    onChangeType={(e)=>handleChangeInput(e, 'UPDATE_URL_TYPE')}
-                    onChange={(e)=>handleChangeInput(e, 'UPDATE_URL_INPUT')}
-                    addInput={()=>handleAddInput('ADD_URL')} 
-                    removeInput={(id)=>handleRemoveInput(id, 'REMOVE_URL')}
+                    title="Websites" 
+                    container={state.inputs.websites}
+                    // onChangeType={(e)=>handleChangeInput(e, 'UPDATE_URL_TYPE')}
+                    onChange={(e)=>handleChangeInput(e, 'UPDATE_WEBSITES_INPUT')}
+                    addInput={()=>handleAddInput('ADD_WEBSITES')} 
+                    // removeInput={(id)=>handleRemoveInput(id, 'REMOVE_URL')}
                 />
-                <div className="flex-col flex">
-                    <ADRInputRemoveSelect 
-                        {...state.inputs.addresses[0]}
-                        onChange={(e)=>handleChangeInput(e, 'UPDATE_ADDRESS_INPUT')}
-
-                    />
-                    <AddressAddRemoveInput 
-                        container={state.inputs.addresses}
-                        addInput={()=>handleAddInput('ADD_ADDRESS')}
-                        removeInput={(id) => handleRemoveInput(id, 'REMOVE_ADDRESS')}
-                        onChange={(e)=>handleChangeInput(e,'UPDATE_ADDRESS_INPUT')}
-                    />
-                </div>
                 {/* <button onClick={handleGenerateFormat}>Generate TEL</button> */}
-                <QRCode
-                    ref={qrRef}
-                    logoImage={require('../assets/images/logo.png')}
-                    removeQrCodeBehindLogo
-                    eyeRadius={4}
-                    value={`BEGIN:VCARD\r\nVERSION:3.0\r\nEND:VCARD`}
-                    qrStyle="square"
-                    eyeColor="#ED184B"
-                    quietZone={15}
-                    size={256}
-                    fgColor="#ED184B"
-                    bgColor="white"
-                />
-                
+                {state.showDownload && state.toConvert  && <QRCode ecLevel='L' value={state.toConvert} />}
                 <div className="flex-col flex flex-center btn-wrapper">
                 {
                     !state.showDownload ? (
--- src/utils/dataFormat.js
+++ src/utils/dataFormat.js
@@ -7,7 +7,7 @@ const escapeChars = (str, searchValue = /,|;/g, newValue = (ch) => `\\${ch}`) =>
 }
 
 
-export const FN_Format = (fname, mname, lname ) => {
+export const FN_Format = (fname, lname, mname="" ) => {
     const cleanFname = escapeChars(fname);
     const cleanMname = escapeChars(mname);
     const cleanLname = escapeChars(lname);
@@ -22,6 +22,14 @@ export const N_Format = (fname, lname , honorificPrefixes = [], honorificSuffixe
     const strHonorificSuffixes = honorificSuffixes.map((str) => escapeChars(str)).join(',');
     return `N:${cleanLname};${cleanFname};${strHonorificPrefixes};${strHonorificSuffixes}\r\n`;
 }
+
+
+export const ORG_Format = (value) => `ORG:${escapeChars(value)}\r\n`;
+
+export const TITLE_Format = (value) => `TITLE:${escapeChars(value)}\r\n`;
+
+export const ROLE_Format = (value) => `ROLE:${escapeChars(value)}\r\n`;
+
 
 export const ADR_Format = (street, city, state, zip, country,type, pref = false) => {
     const params = `${pref ? ';PREF=1' : ''}${type ? `;${type}` : ''}`;
@@ -49,14 +57,14 @@ export const TEL_Format = (type, value) => {
 
 */
     const params = `${type ? ';TYPE=' + type: ''}`;
-    return `TEL${params}:${value}`;
+    return `TEL${params}:${escapeChars(value)}`;
 }
 
 export const URL_Format = (value) => {
-    return `URL:${value}`;
+    return `URL:${escapeChars(value)}`;
 }
 
-export const EMAIL_Format = (type, value) => {
+export const EMAIL_Format = (value ) => {
 /*  
     TYPE parameter values can include:
         -internet : indicate an Internet addressing type
@@ -66,9 +74,12 @@ export const EMAIL_Format = (type, value) => {
     can also be specified.
 */
     // const params = `${type ? 'TYPE=' + type: ''}`;
-    return `EMAIL:${value}`;
+    return `EMAIL:${escapeChars(value)}`;
 }
 
+export const X_SOCIAL_PROFILE_Format = (type, value) => {
+    return `X-SOCIAL-PROFILE:${escapeChars(value)}`;
+}
 export const makeFormat = (format, data) => {
     /* 
     Parameter values MAY be case-sensitive or case-insensitive, 
@@ -76,22 +87,36 @@ export const makeFormat = (format, data) => {
     the same as "FN" and "Fn"). It is RECOMMENDED that property and
     parameter names be upper-case on output.
     */
+
+    console.log(format);
     switch (format) {
         case 'FN':
-            return FN_Format(data.fname, data.mname, data.lname);
+            return FN_Format(data.fname, data.lname);
         case 'N':
-            return N_Format(data.lname,data.fname, data.honorificPrefixes, data.honorificSuffixes);
+            return N_Format(data.lname,data.fname);
+        case 'ORG':
+            return ORG_Format(data);
+        case 'TITLE':
+            return TITLE_Format(data);
+        case 'ROLE':
+            return ROLE_Format(data);
         case 'ADR':
             return data.map(({street, city, state, zip, country, type, pref}) => 
                 ADR_Format(street, city, state, zip, country, type, pref)).join('\r\n');
-        case 'TEL':
+        case 'TELS':
             return data.map( ({type, value}) => TEL_Format(type, value)).join('\r\n');
+        case 'TEL':
+            return TEL_Format('',data) + '\r\n';
         case 'URL':
-            return data.map( ({value}) => URL_Format(value)).join('\r\n');
-            
+            return data.map(({value}) => URL_Format(value)).join('\r\n');
+        case 'EMAILS':
+            return data.map(({type, value}) => EMAIL_Format(type, value)).join('\r\n');
         case 'EMAIL':
-            return data.map( ({type, value}) => EMAIL_Format(type, value)).join('\r\n');
-            
+            return EMAIL_Format(data) + '\r\n';
+        case 'X-SOCIAL-PROFILES':
+            return data.map(({type, value}) => X_SOCIAL_PROFILE_Format(type, value)).join('\r\n');
+        case 'X-SOCIAL-PROFILE':
+            return X_SOCIAL_PROFILE_Format('',data)
         default:
             break;
     }
